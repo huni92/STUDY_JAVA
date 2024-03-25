5a05c4f9e449dc1e671f5632962d0339a0d4849e
DESKTOP-F4BC13L\hi <gns2862@naver.com>
2024-03-25 11:17:11 +0900

    챕터03 scanner commit 2

--- 01-lecture-source/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section04/scanner/Application1.java
+++ 01-lecture-source/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section04/scanner/Application1.java
@@ -14,31 +14,38 @@ public class Application1 {
         // java.util.Scanner sc = new java.util.Scanner(java.lang.System.in); //java.lang 생략해도 사용 가능!
 
         /* 1-2. 다른 클래스에 클래스 사용 시 패키지명 생략하기 위해 사용하는구문 사용(import) */
-        Scanner sc1 = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         /* 2. 자료형 값 입력 받기 */
         /* 2-1. 문자열 입력 받기 */
         /* nextLine() : 입력 받은 값을 문자열로 반환한다. */
         System.out.print("이름을 입력해주세요 : ");
-        String name = sc1.nextLine();
+        String name = sc.nextLine();
         System.out.println("입력하신 이름은 " + name + "입니다.");
 
         /* 2-2. 정수값 입력 */
         /* nextInt() : 입력 받은 값을 int형으로 반환한다. */
         System.out.print("나이를 입력하세요 : ");
-        int age = sc1.nextInt();
+        int age = sc.nextInt();
         System.out.println("입력하신 나이는 " + age + "입니다.");
 
         /* 2-3. 실수값 입력받기 */
         /* nextDouble() : 입력 받은 값을 double로 변환 */
         System.out.print("원하는 실수를 입력해주세요 : ");
-        double num = sc1.nextDouble();
+        double num = sc.nextDouble();
         System.out.println("입력하신 실수는 " + num + "입니다.");
 
         /* 2-4. 논리형 값 입력 받기 */
         /* nextBoolean() : 입력받은 값을 boolean으로 변환시켜준다. */
         System.out.print("참과 거짓의 한가지를 true of false로 입력해주세요 : ");
-        boolean isTrue = sc1.nextBoolean();
+        boolean isTrue = sc.nextBoolean();
         System.out.println("입력하신 논리값은 " + isTrue + "입니다.");
+
+        /* 2-5. 문자형 값 입력 받기 */
+        /* charAt(int index)를 사용한다. */
+        sc.nextLine();
+        System.out.print("아무 문자나 입력해주세요 : ");
+        char ch = sc.nextLine().charAt(0);
+        System.out.println("입력하신 문자는 " + ch + "입니다.");
     }
 }
